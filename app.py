12e895f72128857ac0ee002d8921df7603c88929
light5551 <sergey.glazunov.99@mail.ru>
2021-03-04 21:24:49 +0300

    add 1st version

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, redirect, url_for
+from flask_dance.contrib.github import make_github_blueprint, github
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
+app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
+app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
+github_bp = make_github_blueprint()
+app.register_blueprint(github_bp, url_prefix="/login")
+
+
+
+@app.route("/")
+def index():
+    return "check"
+    '''
+    if not github.authorized:
+        return redirect(url_for("github.login"))
+    resp = github.get("/user")
+    if resp.ok:
+        return "You are @{login} on GitHub".format(login=resp.json()["login"])
+    return "<h1>Ooops!</h1>"
+    '''
+    
