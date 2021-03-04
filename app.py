d6dc6b34a163118d23c8530908106d8ef215480d
light5551 <sergey.glazunov.99@mail.ru>
2021-03-04 21:43:41 +0300

    1st fix heroku

--- app.py
+++ app.py
@@ -4,26 +4,24 @@ from flask_dance.contrib.github import make_github_blueprint, github
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
-app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
-app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
+app.config["GITHUB_OAUTH_CLIENT_ID"] = "2d6432d7473dd5d82b4b"
+app.config["GITHUB_OAUTH_CLIENT_SECRET"] = "c5ab4101055c32325ce838504691a1577401a346"
 github_bp = make_github_blueprint()
-app.register_blueprint(github_bp, url_prefix="/login")
+app.register_blueprint(github_bp, url_prefix="/github_login")
 
 
 
 @app.route("/")
 def index():
-    return "<h1>check</h1>"
+    if not github.authorized:
+        return redirect(url_for("github.login"))
+    resp = github.get("/user")
+    if resp.ok:
+        return "You are @{login} on GitHub".format(login=resp.json()["login"])
+    return "<h1>Ooops!</h1>"
 
 
 if __name__ == '__main__': app.run(debug=True)
 
-    #'''
-    #if not github.authorized:
-    #    return redirect(url_for("github.login"))
-    #resp = github.get("/user")
-    #if resp.ok:
-    #    return "You are @{login} on GitHub".format(login=resp.json()["login"])
-    #return "<h1>Ooops!</h1>"
-    #'''
+    
     
